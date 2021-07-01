a0ab75189686685948df16c29cc3a874d2d2ee7e
ffh2000 <ffh2000@mail.ru>
2021-07-01 22:39:05 +0300

    '.'

--- app/src/main/java/ru/mstrike/popit/MainActivity.kt
+++ app/src/main/java/ru/mstrike/popit/MainActivity.kt
@@ -2,33 +2,49 @@ package ru.mstrike.popit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.transition.ChangeBounds
-import android.transition.TransitionManager
-import android.view.SurfaceControl
-import android.view.animation.AccelerateDecelerateInterpolator
+import android.util.Log
 import android.view.animation.AnimationUtils
-import android.view.animation.AnticipateInterpolator
-import android.view.animation.Interpolator
-import android.widget.ImageView
-import androidx.constraintlayout.solver.state.State
-import androidx.constraintlayout.widget.ConstraintSet
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.start_layout.*
+import android.view.View
+import kotlinx.coroutines.*
+import ru.mstrike.popit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     private fun startAnimations() {
         val anim = AnimationUtils.loadAnimation(this, R.anim.bg_animation)
-        bground.startAnimation(anim)
-        startPicture.startAnimation(AnimationUtils.loadAnimation(this, R.anim.start_button))
-
-        startBackgound.setOnClickListener {
-            vfMainMenu.showNext()
-        }
+        binding.bground.startAnimation(anim)
+        binding.incStart.ivStart.startAnimation(AnimationUtils.loadAnimation(this, R.anim.start_button))
     }
+
+    private fun setupListeners() {
+        //нажатие кнопки "Старт"
+        Log.d("MainActivityThread", "onCreate: current thread: ${Thread.currentThread()}")
+        binding.incStart.ivStart.setOnClickListener {
+            binding.vfMainMenu.showNext()
+        }
+        binding.incGameOptions.incOption1.ivDesk1.setOnClickListener(gameDeskSelectListener)
+        binding.incGameOptions.incOption1.ivDesk2.setOnClickListener(gameDeskSelectListener)
+        binding.incGameOptions.incOption1.ivDesk3.setOnClickListener(gameDeskSelectListener)
+        binding.incGameOptions.incOption1.ivDesk4.setOnClickListener(gameDeskSelectListener)
+
+    }
+
+    /**
+     * Обработчик клика при выборе вида игровой доски
+     */
+    private val gameDeskSelectListener = { _: View ->
+        binding.incGameOptions.vfGameOptions.showNext()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        startAnimations()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        GlobalScope.launch(Dispatchers.IO) {
+            startAnimations()
+            setupListeners()
+        }
     }
 }
