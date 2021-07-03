cd5536ceb2fc80eb204e9beb55919fe6490293c8
ffh2000 <ffh2000@mail.ru>
2021-07-03 10:12:09 +0300

    '.'

--- app/src/main/java/ru/mstrike/popit/common/PopitDesktop.kt
+++ app/src/main/java/ru/mstrike/popit/common/PopitDesktop.kt
@@ -2,6 +2,7 @@ package ru.mstrike.popit.common
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.*
 import androidx.constraintlayout.widget.ConstraintLayout
 
 /**
@@ -9,8 +10,44 @@ import androidx.constraintlayout.widget.ConstraintLayout
  */
 class PopitDesktop(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : ConstraintLayout(context, attrs, defStyleAttr) {
 
+    /**
+     * Игровая матрица в которой хранятся состояния ячеек
+     */
+    var gameMatrix: Array<Array<Int>> = arrayOf()
+
+    /**
+     * Матрица с элементами управления, отображаемыми на экране.
+     * Размер матрицы и картинки элементов должны соответствовать размеру
+     * и состояниям ячеек матрицы [gameMatrix]
+     */
+    var viewsMatrix: Array<Array<View?>> = arrayOf()
+
     constructor(context: Context, attrs: AttributeSet?): this(context, attrs, 0)
 
     constructor(context: Context): this(context, null)
+
+    init {
+        initGameMatrix(4, 4)
+    }
+
+    /**
+     * Метод инициализирует и заполняет 0 игровую матрицу [gameMatrix]
+     * @param rows кол-во создаваемых строк в матрице
+     * @param columns кол-во создаваемых колонок в матрице
+     */
+    fun initGameMatrix(rows: Int, columns: Int) {
+        gameMatrix = arrayOf()
+        viewsMatrix = arrayOf()
+        for (i in 0 until rows) {
+            val a = arrayOf<Int>()
+            val v = arrayOf<View?>()
+            for (j in 0 until columns) {
+                a[j] += 0
+                v[j] = null
+            }
+            gameMatrix += a
+            viewsMatrix += v
+        }
+    }
 
 }
